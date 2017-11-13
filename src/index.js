5621533c2e4f2a50cb4ee05d03d9cfaead82ffa0
marcosjrs <marcos.rivera@tecnocom.es>
2017-11-13 11:57:04 +0100

    Estado, reductor y store . Encapsulado del componente principal (App) con Provider con atributo Store.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,31 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+//ESTADO
+const estado = {
+    numero: 1
+};
+
+//REDUCTORES
+const reductores = (estado,accion) =>{
+    switch (accion.type) {
+        case "AUMENTAR":
+            var newEstado = Object.assign({},estado);
+            ++newEstado.numero;
+            return newEstado;
+        case "DISMINUIR":
+            var newEstado = Object.assign({},estado);
+            --newEstado.numero;
+            return newEstado;
+    }
+    return estado;
+};
+
+//STORE
+const store = createStore(reductores, estado);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
